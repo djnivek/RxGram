905a8ce54afcbe2b42a6cd3fc7ba895cc751ed4d
Kevin Machado <machadokevin.dev@gmail.com>
2019-01-21 16:06:47 +0100

    starting initial architecture

---
+++ RxGram/Modules/Explore/Views/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  RxGram
+//
+//  Created by MACHADO KEVIN on 21/01/2019.
+//  Copyright © 2019 Kevin Machado. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
